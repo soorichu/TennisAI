ff389c0636a04bab0831d274b6de15f4824510b9
Soori <soorichu@gmail.com>
2021-09-10 16:09:20 +0900

    하다만

---
+++ Assets/Scripts/HitWall.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+
+public class HitWall : MonoBehaviour
+{
+    public GameObject areaObject;
+    public int lastAgentHit;
+    public bool net;
+
+    public enum FloorHit
+    {
+        Service,
+        FloorHitUnset,
+        FloorAHit,
+        FloorBHit
+    }
+
+    public FloorHit lastFloorHit;
+
+    TennisArea m_Area;
+    TennisAgent m_AgentA;
+    TennisAgent m_AgentB;
+
+    void Start()
+    {
+        m_Area = areaObject.GetComponent<TennisArea>();
+        m_AgentA = m_Area.agentA.GetComponent<TennisAgent>();
+        m_AgentB = m_Area.agentB.GetComponent<TennisAgent>();
+    }
+
+    private void Reset()
+    {
+        m_AgentA.EndEpisode();
+        m_AgentB.EndEpisode();
+        m_Area.MatchReset();
+        lastFloorHit = FloorHit.Service;
+        net = false;
+    }
+
+    void AgentAWins()
+    {
+        m_AgentA.SetReward(1);
+        m_AgentB.SetReward(-1);
+        m_AgentA.score += 1;
+        Reset();
+    }
+
+    void AgentBWins()
+    {
+        m_AgentA.SetReward(-1);
+        m_AgentB.SetReward(1);
+        m_AgentB.score += 1;
+        Reset();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("iWall"))
+        {
+            if (collision.gameObject.name == "wallA")
+            {
+                if (lastAgentHit == 0 || lastFloorHit == FloorHit.FloorAHit)
+                {
+                    AgentBWins();
+                }
+                else
+                {
+                    AgentAWins();
+                }
+            }
+            else if(collision.gameObject.name == "wallB")
+            {
+                if(lastAgentHit == 1 || lastFloorHit == FloorHit.FloorBHit)
+                {
+                    AgentAWins();
+                }
+                else
+                {
+                    AgentBWins();
+                }
+            }
+            else if(collision.gameObject.name == "floorA")
+            {
+                if(lastAgentHit == 0 || lastFloorHit == FloorHit.FloorAHit || lastFloorHit == FloorHit.Service)
+                {
+                    AgentBWins();
+                }
+                else
+                {
+                    lastFloorHit = FloorHit.FloorAHit;
+                    if (!net)
+                    {
+                        net = true;
+                    }
+                }
+            }
+            else if(collision.gameObject.name == "net" && !net)
+            {
+                if(lastAgentHit == 0)
+                {
+                    AgentBWins();
+                }
+                else if(lastAgentHit == 1)
+                {
+                    AgentAWins();
+                }
+            }
+        }
+        else if(collision.gameObject.name == "AgentA")
+        {
+            if(lastAgentHit == 0)
+            {
+                AgentBWins();
+            }
+            else
+            {
+                if(lastFloorHit != FloorHit.Service && !net)
+                {
+                    net = true;
+                }
+
+                lastAgentHit = 0;
+                lastFloorHit = FloorHit.FloorHitUnset;
+            }
+        }
+        else if(collision.gameObject.name == "AgentB")
+        {
+            if(lastAgentHit == 1)
+            {
+                AgentAWins();
+            }
+            else
+            {
+                if(lastFloorHit != FloorHit.Service && !net)
+                {
+                    net = true;
+                }
+                lastAgentHit = 1;
+                lastFloorHit = FloorHit.FloorHitUnset;
+            }
+        }
+    }
+}
